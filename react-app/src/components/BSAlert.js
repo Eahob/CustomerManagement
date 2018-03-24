831fbd3f4d42f81b5fb5cd9dab5566beb8bb991c
Eahob <lerbfae@gmail.com>
2018-03-24 21:54:08 +0100

    added a fade-in effect to the alert component

--- react-app/src/components/BSAlert.js
+++ react-app/src/components/BSAlert.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 function BSAlert(props) {
     if (!props.stt.responseStatus) return null
-    
+
     return (
-        <div className={' alert alert-' + (props.stt.error ? 'danger' : 'success')} role="alert">
+        <div className={'BSAlert alert alert-' + (props.stt.error ? 'danger' : 'success')} role="alert">
             <h4>{props.stt.error ? props.alertError : props.alertSuccess}</h4>
             {props.stt.error && <p>{props.stt.error}</p>}
         </div>
