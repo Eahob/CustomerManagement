1a737e53051e8362c8a47e5f91a2364eb009eac7
Eahob <lerbfae@gmail.com>
2018-03-24 21:22:11 +0100

    code cleaning

--- react-app/src/components/BSAlert.js
+++ react-app/src/components/BSAlert.js
@@ -1,9 +1,8 @@
 import React from 'react'
 
 function BSAlert(props) {
-    if (!props.stt.responseStatus) {
-        return null
-    }
+    if (!props.stt.responseStatus) return null
+    
     return (
         <div className={' alert alert-' + (props.stt.error ? 'danger' : 'success')} role="alert">
             <h4>{props.stt.error ? props.alertError : props.alertSuccess}</h4>
