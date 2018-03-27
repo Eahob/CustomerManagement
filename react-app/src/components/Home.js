cdb5f2602070d4142cfef71a7fe2685debc87829
Eahob <lerbfae@gmail.com>
2018-03-27 14:50:18 +0200

    updated home page

--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 function Home(props) {
     return (
-        <div id="home">
-            lol
+        <div id="home" className="text-white d-flex flex-column">
+            <h1 className="inline-block text-center display-4 mb-4"><em>Customer Management demo</em></h1>
+            <div className="mx-auto">
+                <p>Here you can create new customers, search them, modify them and consult what they did</p>
+                <p>You can search modify and add products or services for your customers in the top menu</p>
+            </div>
+            <footer class="inline-block text-center mt-auto text-secondary">
+                <p>Developed by <a className="badge badge-secondary" href="https://github.com/Eahob">Eahob</a></p>
+            </footer>
         </div>
     )
 }
