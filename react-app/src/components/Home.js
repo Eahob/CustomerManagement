065b5ef996d90ccbbbea3acfa63cf069d0c5b0da
Eahob <lerbfae@gmail.com>
2018-03-27 12:02:45 +0200

    added home page

---
+++ react-app/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home(props) {
+    return (
+        <div id="home">
+            lol
+        </div>
+    )
+}
+
+export default Home
--- react-app/src/components/PrimaryLayout.js
+++ react-app/src/components/PrimaryLayout.js
@@ -9,27 +9,29 @@ import CreateAndEditCustomer from './CreateAndEditCustomer'
 import CreateAndEditTicket from './CreateAndEditTicket'
 import CreateAndEditService from './CreateAndEditService'
 import CreateAndEditProduct from './CreateAndEditProduct'
+import Home from './Home'
 
 function PrimaryLayout() {
-  return (
-    <div>
-      <PrimaryHeader />
-      <Switch>
-        <Route path="/customers" component={Customers} />
-        <Route path="/tickets" component={Tickets} />
-        <Route path="/services" component={Services} />
-        <Route path="/products" component={Products} />
-        <Route path="/new/customer" component={CreateAndEditCustomer} />
-        <Route path="/new/ticket" component={CreateAndEditTicket} />
-        <Route path="/new/service" component={CreateAndEditService} />
-        <Route path="/new/product" component={CreateAndEditProduct} />
-        <Route path="/customer/:id" component={CreateAndEditCustomer} />
-        <Route path="/ticket/:id" component={CreateAndEditTicket} />
-        <Route path="/service/:id" component={CreateAndEditService} />
-        <Route path="/product/:id" component={CreateAndEditProduct} />
-      </Switch>
-    </div>
-  )
+    return (
+        <div>
+            <PrimaryHeader />
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/customers" component={Customers} />
+                <Route path="/tickets" component={Tickets} />
+                <Route path="/services" component={Services} />
+                <Route path="/products" component={Products} />
+                <Route path="/new/customer" component={CreateAndEditCustomer} />
+                <Route path="/new/ticket" component={CreateAndEditTicket} />
+                <Route path="/new/service" component={CreateAndEditService} />
+                <Route path="/new/product" component={CreateAndEditProduct} />
+                <Route path="/customer/:id" component={CreateAndEditCustomer} />
+                <Route path="/ticket/:id" component={CreateAndEditTicket} />
+                <Route path="/service/:id" component={CreateAndEditService} />
+                <Route path="/product/:id" component={CreateAndEditProduct} />
+            </Switch>
+        </div>
+    )
 }
 
 export default PrimaryLayout
