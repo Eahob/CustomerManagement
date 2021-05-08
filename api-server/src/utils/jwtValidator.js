39ee9c084bfc3e8f7c3ed8c154e7134a6cb29c6e
Eahob <lerbfae@gmail.com>
2021-05-08 16:59:28 +0200

    Add a way to dissable token validation

--- api-server/src/utils/jwtValidator.js
+++ api-server/src/utils/jwtValidator.js
@@ -1,17 +1,21 @@
 const { failResponse } = require('./api-utils')
 const jwt = require('jsonwebtoken')
 
-const secret = process.env.JWT_SECRET 
+const secret = process.env.JWT_SECRET
 
-function jwtValidator(req, res, next) {
-    const auth = req.get('authorization')
-    try {
-        const token = auth.split(' ')[1]
-        jwt.verify(token, secret)
-        next()
-    } catch(err) {
-        res.json(failResponse('invalid token'))
-    }
+const jwtValidator = (req, res, next) => {
+	const auth = req.get('authorization')
+	try {
+		const token = auth.split(' ')[1]
+		jwt.verify(token, secret)
+		next()
+	} catch(err) {
+		res.json(failResponse('invalid token'))
+	}
 }
 
-module.exports = jwtValidator
+const doNothing = (req, res, next) => {
+	next()
+};
+
+module.exports = Number(process.env.DISSABLE_VALIDATOR) ? doNothing : jwtValidator;
