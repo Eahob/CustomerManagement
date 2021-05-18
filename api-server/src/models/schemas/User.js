c7cfcc148e44effe00ab01ef7d91784f4ddd186b
Eahob <lerbfae@gmail.com>
2021-05-18 20:19:17 +0200

    Update user schema

--- api-server/src/models/schemas/User.js
+++ api-server/src/models/schemas/User.js
@@ -1,16 +1,31 @@
 import mongoose from 'mongoose';
+import uniqueValidator from 'mongoose-unique-validator';
 
-export const userSchema = new mongoose.Schema({
+const stringDoesNotHaveInnerSpaces = string => !/[\s\uFEFF\xA0]+/.test(string);
+
+const userSchema = new mongoose.Schema({
 	username: {
 		type: String,
 		unique: true,
 		required: true,
 		trim: true,
-		uppercase: true
+		uppercase: true,
+		validate: {
+			validator: stringDoesNotHaveInnerSpaces,
+			message: 'No spaces allowed in username'
+		}
 	},
 	password: {
 		type: String,
 		trim: true,
-		required: true
+		required: true,
+		validate: {
+			validator: stringDoesNotHaveInnerSpaces,
+			message: 'No spaces allowed in password'
+		}
 	}
 });
+
+userSchema.plugin(uniqueValidator);
+
+export { userSchema };
