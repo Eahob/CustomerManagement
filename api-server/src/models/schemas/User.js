412143645b0f918378e81e7b8df435feb15e2293
Eahob <lerbfae@gmail.com>
2021-05-18 18:20:12 +0200

    Trim username and password

--- api-server/src/models/schemas/User.js
+++ api-server/src/models/schemas/User.js
@@ -5,10 +5,12 @@ export const userSchema = new mongoose.Schema({
 		type: String,
 		unique: true,
 		required: true,
+		trim: true,
 		uppercase: true
 	},
 	password: {
 		type: String,
+		trim: true,
 		required: true
 	}
 });
